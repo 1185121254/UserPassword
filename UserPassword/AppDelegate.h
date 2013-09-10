5034f0354f81ba10d7df0f614fc04b43225e3182
administrator <administrator@huanglianglei.local>
2013-09-10 11:46:22 +0800

    Initial Commit

---
+++ UserPassword/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  UserPassword
+//
+//  Created by administrator on 13-9-10.
+//  Copyright (c) 2013年 administrator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
